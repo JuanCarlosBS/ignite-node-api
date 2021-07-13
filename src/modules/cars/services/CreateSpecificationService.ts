c4df21ee20280bc8e40e6a507b7bd008c413b94b
juancarlosbs <juanrov_@hotmail.com>
2021-07-13 11:27:02 -0300

    feat: create specifications

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,20 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+class CreateSpecificationService {
+    constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+    execute({ name, description }) {
+        const specificationAlreadyExists =
+            this.specificationsRepository.findByName(name);
+
+        if (specificationAlreadyExists) {
+            throw new Error("Specification already exists!");
+        }
+        this.specificationsRepository.create({
+            name,
+            description,
+        });
+    }
+}
+
+export { CreateSpecificationService };
